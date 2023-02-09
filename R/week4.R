68062aaf1e7d05bfd303babd912943da59fdb740
Saron M Demeke <demek004@umn.edu>
2023-02-08 22:11:30 -0600

    done through line 9

--- R/week4.R
+++ R/week4.R
@@ -1,3 +1,9 @@
 #Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
+
+#Data Import
+import_tbl <- read_delim("../data/week4.dat",delim="-",col_names = c("casenum","parnum","stimver","datadate","qs") )
+glimpse(import_tbl)
+wide_tbl <- import_tbl %>% separate(qs, into = c("q1","q2","q3","q4","q5"), sep = " - ") 
+wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], as.integer)
