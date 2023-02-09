03128ee21fe87b6ca64eea7c25c6b4f8b8e29ab3
Saron M Demeke <demek004@umn.edu>
2023-02-08 22:12:04 -0600

    done through line 11

--- R/week4.R
+++ R/week4.R
@@ -7,3 +7,5 @@ import_tbl <- read_delim("../data/week4.dat",delim="-",col_names = c("casenum","
 glimpse(import_tbl)
 wide_tbl <- import_tbl %>% separate(qs, into = c("q1","q2","q3","q4","q5"), sep = " - ") 
 wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], as.integer)
+wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %H:%M:%S")
+wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], na_if, 0)
