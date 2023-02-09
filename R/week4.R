88276ad918bf72ddfa6cf15f2682ca52b597aa49
Saron M Demeke <demek004@umn.edu>
2023-02-08 22:14:02 -0600

    finished proj

--- R/week4.R
+++ R/week4.R
@@ -9,3 +9,9 @@ wide_tbl <- import_tbl %>% separate(qs, into = c("q1","q2","q3","q4","q5"), sep 
 wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], as.integer)
 wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %H:%M:%S")
 wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], na_if, 0)
+
+
+
+
+wide_tbl <- wide_tbl %>% drop_na(q2)
+long_tbl <- wide_tbl %>% pivot_longer(cols = starts_with("q"), names_to = "ques",values_to = "response")
