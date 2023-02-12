84035a449a59192f6cc53a9a25e9b299949ffd1e
Saron M Demeke <demek004@umn.edu>
2023-02-12 11:18:01 -0600

    fixed pipes

--- R/week4.R
+++ R/week4.R
@@ -5,13 +5,13 @@ library(tidyverse)
 #Data Import
 import_tbl <- read_delim("../data/week4.dat",delim="-",col_names = c("casenum","parnum","stimver","datadate","qs") )
 glimpse(import_tbl)
-wide_tbl <- import_tbl %>% separate(qs, into = c("q1","q2","q3","q4","q5"), sep = " - ") 
+wide_tbl <- separate(import_tbl,qs, into = c("q1","q2","q3","q4","q5"), sep = " - ") 
 wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], as.integer)
 wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %H:%M:%S")
-wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], na_if, 0)
+wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], na_if, 0L)
 
 
 
 
-wide_tbl <- wide_tbl %>% drop_na(q2)
-long_tbl <- wide_tbl %>% pivot_longer(cols = starts_with("q"), names_to = "ques",values_to = "response")
+wide_tbl <- drop_na(wide_tbl,q2)
+long_tbl <- pivot_longer(wide_tbl, cols = starts_with("q"), names_to = "ques",values_to = "response")
